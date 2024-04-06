269e27f548ac69b3089417b4539153ea95ef8774
StefanGlova <glovastefan@protonmail.com>
2024-04-06 21:14:33 +0100

    new fail test - test for vehicle type which is neither trailer nor rigid. Also test directly ParseCSV class and its method parse.

--- tests/test_order_details.py
+++ tests/test_order_details.py
@@ -1,5 +1,6 @@
 import unittest
 from order_parser import OrderParser
+from parse_csv import ParseCSV
 
 
 class TestOrderDetails(unittest.TestCase):
@@ -11,9 +12,14 @@ class TestOrderDetails(unittest.TestCase):
             )
             file.write("Alice,E1W 2RG,SKU123,57,trailer,trailer,2024-04-10\n")
             file.write("Bob,N9 9LA,SKU456,1000,rigid,2023-11-10\n")
+            file.write("Error Vehicle Type,SKU2,5,wrong vehicle,2023-11-10\n")
+
+        # parse csv file
+        orderbook_file = ParseCSV("orderbook.csv")
+        parsed_data = orderbook_file.parse()
 
         # initialize OrderParser
-        parser = OrderParser("orderbook.csv")
+        parser = OrderParser(parsed_data)
 
         # parse the orderbook
         orders_by_vehicle = parser.parse_orderbook()
@@ -23,6 +29,10 @@ class TestOrderDetails(unittest.TestCase):
         self.assertEqual(len(orders_by_vehicle["rigid"]), 1)
         self.assertEqual(orders_by_vehicle["trailer"][0]["Customer Name"], "Alice")
         self.assertEqual(orders_by_vehicle["rigid"][0]["Customer Postcode"], "N9 9LA")
+        self.assertEqual(len(orders_by_vehicle["ERROR"]), 1)
+        self.assertEqual(
+            orders_by_vehicle["ERROR"][0]["Customer Name"], "Error Vehicle Type"
+        )
 
 
 if __name__ == "__main__":
