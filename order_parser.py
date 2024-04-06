2a2e3a15806411b74e3708264bac60854c66949f
StefanGlova <glovastefan@protonmail.com>
2024-04-06 21:02:16 +0100

    code refactored by separating parse csv functionality to its own class ParseCSV which can be used later again. Class OrderParser refactored to make it pass the test.

--- order_parser.py
+++ order_parser.py
@@ -1,36 +1,32 @@
+from parse_csv import ParseCSV
+
+
 class OrderParser:
     def __init__(self, file_path):
         self.file_path = file_path
 
     def parse_orderbook(self):
-        # import csv module
-        import csv
+
+        parsed_data = ParseCSV(self.file_path).parse_csv()
 
         # create variable orderbook for dictionary datastructure which stores data from orderbook file
         orderbook = {"trailer": list(), "rigid": list(), "ERROR": list()}
 
-        # open orderbook file
-        with open(self.file_path, "r") as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                customer_name = row["Customer Name"]
-                postcode = row["Customer Postcode"]
-                sku = row["SKU"]
-                qty = row["Qty"]
-                vehicle = row["Vehicle Type"]
-                date = row["Due Date"]
-                customer = {
-                    "Customer Name": customer_name,
-                    "Customer Postcode": postcode,
-                    "SKU": sku,
-                    "Qty": qty,
-                    "Due Date": date,
-                }
-                if vehicle == "trailer":
-                    orderbook["trailer"].append(customer)
-                elif vehicle == "rigid":
-                    orderbook["rigid"].append(customer)
-                else:
-                    pass
+        for row in parsed_data:
+            customer = {
+                "Customer Name": row["Customer Name"],
+                "Customer Postcode": row["Customer Postcode"],
+                "SKU": row["SKU"],
+                "Qty": row["Qty"],
+                "Due Date": row["Due Date"],
+            }
+            vehicle = row["Vehicle Type"]
+
+            if vehicle == "trailer":
+                orderbook["trailer"].append(customer)
+            elif vehicle == "rigid":
+                orderbook["rigid"].append(customer)
+            else:
+                pass
 
         return orderbook
---
+++ parse_csv.py
@@ -0,0 +1,14 @@
+import csv
+
+
+class ParseCSV:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def parse_csv(self):
+        parsed_data = []
+        with open(self.file_path, "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                parsed_data.append(row)
+        return parsed_data
