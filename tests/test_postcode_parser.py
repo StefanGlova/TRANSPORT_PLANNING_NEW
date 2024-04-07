202dc9b81b3bf55129d714e19361bed6b731829e
StefanGlova <glovastefan@protonmail.com>
2024-04-07 17:08:19 +0100

    docstring and comments added to postcode_parser.py and test_postcode_parser.py

--- tests/test_postcode_parser.py
+++ tests/test_postcode_parser.py
@@ -8,15 +8,28 @@ class TestPostcodeParser(unittest.TestCase):
     """
 
     def test_postcode_parser(self) -> None:
+        """
+        Test parsing list of dictionaries.
 
+        This test case verifies that PostcodeParser class correctly parse and organised input list of dictionaries and return dictionary, which correctly process latitude and longitude for each postcode.
+
+
+        It checks:
+        - Number of elements in postcode dict
+        - Correct nasted datastructure of postcode dict where top level key is postcode with value of dict with two keys - latitude and longitude
+        - Latitude of each postcode is correct value and type (float)
+        - Longitude of each postcode is correct value and type (float)
+        """
+        # Create parsed_data list of dicts for testing purpose
         parsed_data = [
             {"Postcode": "ABC", "Latitude": "1.123456", "Longitude": "50.123456"},
             {"Postcode": "EFG", "Latitude": "1.987654", "Longitude": "50.654987"},
         ]
-
+        # Initialize PostcodeParser object
         parser = PostcodeParser(parsed_data)
+        # Parse the postcodes
         postcodes = parser.parse_postcodes()
-
+        # Verify parsed data
         self.assertEqual(len(postcodes), 2)
         self.assertEqual(postcodes["ABC"]["Latitude"], 1.123456)
         self.assertEqual(postcodes["ABC"]["Longitude"], 50.123456)
