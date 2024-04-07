cefc7eb9b591bd0903fe5f792787a9d5edf64811
StefanGlova <glovastefan@protonmail.com>
2024-04-07 16:57:31 +0100

    PostcodeParser class implemented with parse_postcode method which passed the test

--- postcode_parser.py
+++ postcode_parser.py
@@ -1,1 +1,14 @@
+class PostcodeParser:
+    def __init__(self, parsed_data: list):
+        self.parsed_data = parsed_data
 
+    def parse_postcodes(self) -> dict:
+
+        postcodes = dict()
+
+        for row in self.parsed_data:
+            postcodes[row["Postcode"]] = dict()
+            postcodes[row["Postcode"]]["Latitude"] = float(row["Latitude"])
+            postcodes[row["Postcode"]]["Longitude"] = float(row["Longitude"])
+
+        return postcodes
