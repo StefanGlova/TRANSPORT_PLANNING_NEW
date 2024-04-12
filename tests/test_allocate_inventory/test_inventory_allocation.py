51aba118ab44cceb4119f038f8e1a63489ddc2dc
StefanGlova <glovastefan@protonmail.com>
2024-04-12 20:34:25 +0100

    Initial simple case test created for Inventory Allocation. Test failed - no InventoryAllocation class created yet.

---
+++ tests/test_allocate_inventory/test_inventory_allocation.py
@@ -0,0 +1,37 @@
+import unittest
+from datetime import datetime
+
+
+class TestInventoryAllocation(unittest.TestCase):
+    """
+    Test suit for Inventory Allocation
+    """
+
+    def test_inventory_allocation_simple_case(self):
+
+        orderbook = {
+            "trailer": [
+                {
+                    "Customer Name": "ABC",
+                    "Customer Postcode": "ABC123",
+                    "SKU": "SKU1",
+                    "Qty": 60,
+                    "Due Date": 2023 - 11 - 10,
+                },
+                {},
+            ],
+            "rigid": [],
+        }
+
+        inventory = {
+            "SKU1": 100,
+        }
+
+        allocator = InventoryAllocation(orderbook, inventory)
+        orderbook_allocated, inventory_left, orderbook_not_allocated = (
+            allocator.allocate_inventory()
+        )
+
+        self.assertEqual(orderbook_allocated["trailer"][0]["Allocated Qty"], 60)
+        self.assertEqual(inventory_left["SKU1"], 40)
+        self.assertEqual(orderbook_not_allocated["trailer"], [])
