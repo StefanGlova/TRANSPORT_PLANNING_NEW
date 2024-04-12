12a9921ee4ae40ffb1fb5ea2ce0e08aeb27f9240
StefanGlova <glovastefan@protonmail.com>
2024-04-12 13:13:49 +0100

    Code refactored to improve modularization - parsing inventory separated to its own class.

--- modules/data_processing_module/parse_csv.py
+++ modules/data_processing_module/parse_csv.py
@@ -68,40 +68,6 @@ class GeneralFileParser:
 
         return parsed_data
 
-    def parse_inventory(self) -> dict:
-        """
-        Parse data from parsed_data list into dictionary and group them by SKU.
-
-        Returns:
-            dictionary: An inventory dictionary of SKU and it qty, grouped by SKU.
-        """
-        # Initialize dict datastructure which stores inventry key value pairs and also variable correct_keys
-        inventory, correct_keys = dict(), ["SKU", "Qty"]
-        fields = {"SKU": "string", "Qty": "number"}
-
-        # Check if parsed_data is not empty list
-        if self.parsed_data == []:
-            raise WrongKeysError(
-                method_called="parse_inventory", correct_keys=correct_keys
-            )
-
-        # Iterate over each item of the parsed_data list
-        for row in self.parsed_data:
-            # Check if row only contains correct keys
-            if sorted(list(set(key for key in row.keys()))) != sorted(correct_keys):
-                raise WrongKeysError(
-                    method_called="parse_inventory", correct_keys=correct_keys
-                )
-            # Check if Qty field has numeric value
-            try:
-                qty = int(row["Qty"])
-            except ValueError:
-                raise WrongValueTypeError("Qty", fields)
-            # If SKU already exist in inventory dict, then it increment its value by Qty, if does not exist, it adds it with its initial Qty
-            inventory[row["SKU"]] = inventory.get(row["SKU"], 0) + qty
-
-        return inventory
-
     def parse_orderbook(self) -> dict:
         """
         Parse data from parsed_data list into dictionary of lists.
---
+++ modules/data_processing_module/process_inventory.py
@@ -0,0 +1,40 @@
+from modules.errors import WrongKeysError, WrongValueTypeError
+
+
+class ProcessInventory:
+    def __init__(self, parsed_data):
+        self.parsed_data = parsed_data
+
+    def parse_inventory(self) -> dict:
+        """
+        Parse data from parsed_data list into dictionary and group them by SKU.
+
+        Returns:
+            dictionary: An inventory dictionary of SKU and it qty, grouped by SKU.
+        """
+        # Initialize dict datastructure which stores inventry key value pairs and also variable correct_keys
+        inventory, correct_keys = dict(), ["SKU", "Qty"]
+        fields = {"SKU": "string", "Qty": "number"}
+
+        # Check if parsed_data is not empty list
+        if self.parsed_data == []:
+            raise WrongKeysError(
+                method_called="parse_inventory", correct_keys=correct_keys
+            )
+
+        # Iterate over each item of the parsed_data list
+        for row in self.parsed_data:
+            # Check if row only contains correct keys
+            if sorted(list(set(key for key in row.keys()))) != sorted(correct_keys):
+                raise WrongKeysError(
+                    method_called="parse_inventory", correct_keys=correct_keys
+                )
+            # Check if Qty field has numeric value
+            try:
+                qty = int(row["Qty"])
+            except ValueError:
+                raise WrongValueTypeError("Qty", fields)
+            # If SKU already exist in inventory dict, then it increment its value by Qty, if does not exist, it adds it with its initial Qty
+            inventory[row["SKU"]] = inventory.get(row["SKU"], 0) + qty
+
+        return inventory
