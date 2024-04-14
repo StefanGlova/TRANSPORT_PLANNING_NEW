63991d48e84b87a03de07469764bd4849ffba4c6
StefanGlova <glovastefan@protonmail.com>
2024-04-14 16:56:35 +0100

    docstring and comments added.

--- modules/allocate_inventory_module/inventory_allocation.py
+++ modules/allocate_inventory_module/inventory_allocation.py
@@ -4,17 +4,26 @@ class InventoryAllocation:
         self.inventory = inventory
 
     def allocate_inventory(self):
-
+        """
+        Allocate inventory method creates 3 dictionaries which are used during method run and returned at the end:
+            - orderbook_allocated - new orderbook variable used for further processing which only include orders with available and allocated inventory
+            - inventory_left - new inventory variable which keep track of change of inventory after the method is finished
+            - orderbook_not_allocated - secondary orderbook, just for reference and further use which shows list of all orders which did not get inventory allocated, as available qty in orderbook was 0
+        """
+        # Initialize dict datastructures for the method output
         orderbook_allocated, inventory_left, orderbook_not_allocated = (
             dict(),
             dict(),
             {"trailer": [], "rigid": [], "error": []},
         )
 
+        # Iterate over each vehicle in orderbook
         for vehicle in self.orderbook:
+            # Iterate over each line of the vehicle in orderbook
             for i in range(len(self.orderbook[vehicle])):
                 sku = self.orderbook[vehicle][i]["SKU"]
                 qty = self.orderbook[vehicle][i]["Qty"]
+                # Check if available inventory is higher or equal then qty needed for order and if so, allocate it, reduce inventory and append order to appropriate vehicle in orderbook_allocated dictionary
                 if self.inventory[sku] >= qty:
                     allocated = qty
                     inventory_left[sku] = (
--- tests/test_allocate_inventory/test_inventory_allocation.py
+++ tests/test_allocate_inventory/test_inventory_allocation.py
@@ -8,9 +8,19 @@ class TestInventoryAllocation(unittest.TestCase):
     """
 
     def test_inventory_allocation_simple_case(self):
+        """
+        Very basic, simple test for checking allocate_inventory method of the class InventoryAllocation.
 
+        This test case create simple orderbook sample and inventory sample with just one order and one SKU in inventory. Available inventory is higher than qty on order.
+
+        It checks:
+        - allocate_inventory correctly crete another key in order dictionary "Allocated Qty" and check that qty assigned to it as value is correct.
+        - inventory left has correct value after allocating inventory to the order
+        - that orderbook_not_allocated does not have any value against vehicle type
+        """
         print("test Allocation simple case")
 
+        # Create orderbook sample with just one order
         orderbook = {
             "trailer": [
                 {
@@ -23,15 +33,22 @@ class TestInventoryAllocation(unittest.TestCase):
             ]
         }
 
+        # Create inventory sample with just one sku
         inventory = {
             "SKU1": 100,
         }
 
+        # Create allocater as object of InventoryAllocation class and run allocate_inventory method on the created object
         allocator = InventoryAllocation(orderbook, inventory)
         orderbook_allocated, inventory_left, orderbook_not_allocated = (
             allocator.allocate_inventory()
         )
 
+        # Verify the outcome of allocate_inventory method
         self.assertEqual(orderbook_allocated["trailer"][0]["Allocated Qty"], 60)
         self.assertEqual(inventory_left["SKU1"], 40)
         self.assertEqual(orderbook_not_allocated["trailer"], [])
+
+
+if __name__ == "__main__":
+    unittest.main()
