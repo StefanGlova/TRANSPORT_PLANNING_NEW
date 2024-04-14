7ad48bc0e0989fb67b832ea7496adb6076d37823
StefanGlova <glovastefan@protonmail.com>
2024-04-14 17:16:10 +0100

    allocata inventory method updated to pass the test.

--- modules/allocate_inventory_module/inventory_allocation.py
+++ modules/allocate_inventory_module/inventory_allocation.py
@@ -7,12 +7,11 @@ class InventoryAllocation:
         """
         Allocate inventory method creates 3 dictionaries which are used during method run and returned at the end:
             - orderbook_allocated - new orderbook variable used for further processing which only include orders with available and allocated inventory
-            - inventory_left - new inventory variable which keep track of change of inventory after the method is finished
             - orderbook_not_allocated - secondary orderbook, just for reference and further use which shows list of all orders which did not get inventory allocated, as available qty in orderbook was 0
+            - inventory - same variable as input, but with updated values
         """
         # Initialize dict datastructures for the method output
-        orderbook_allocated, inventory_left, orderbook_not_allocated = (
-            dict(),
+        orderbook_allocated, orderbook_not_allocated = (
             dict(),
             {"trailer": [], "rigid": [], "error": []},
         )
@@ -26,9 +25,7 @@ class InventoryAllocation:
                 # Check if available inventory is higher or equal then qty needed for order and if so, allocate it, reduce inventory and append order to appropriate vehicle in orderbook_allocated dictionary
                 if self.inventory[sku] >= qty:
                     allocated = qty
-                    inventory_left[sku] = (
-                        inventory_left.get(sku, self.inventory[sku]) - qty
-                    )
+                    self.inventory[sku] -= qty
                     self.orderbook[vehicle][i]["Allocated Qty"] = allocated
 
                     try:
@@ -37,4 +34,4 @@ class InventoryAllocation:
                         orderbook_allocated[vehicle] = []
                         orderbook_allocated[vehicle].append(self.orderbook[vehicle][i])
 
-        return orderbook_allocated, inventory_left, orderbook_not_allocated
+        return orderbook_allocated, self.inventory, orderbook_not_allocated
