90d7dfebdd203f5ea85efca1259529ddfeefceed
StefanGlova <glovastefan@protonmail.com>
2024-04-16 01:18:03 +0100

    ProcessPostcodes class refactored by creating private methods to improve readability of the code.

--- modules/data_processing_module/process_postcodes.py
+++ modules/data_processing_module/process_postcodes.py
@@ -2,57 +2,55 @@ from modules.errors import WrongKeysError, WrongValueTypeError, WrongNumericRang
 
 
 class ProcessPostcodes:
-    def __init__(self, parsed_data):
+    # Initialize global variable for ProcessPostcodes class
+    CORRECT_KEYS = ["Postcode", "Latitude", "Longitude"]
+
+    FIELDS = {
+        "Postcode": "string",
+        "Latitude": "Decimal place number",
+        "Longitude": "Decimal place number",
+    }
+
+    RANGE = {
+        "Latitude": "must be in range from -90 to +90",
+        "Longitude": "must be in range from -180 to +180",
+    }
+
+    def __init__(self, parsed_data: list) -> None:
+        """
+        Initialize ProcessPostcodes object.
+        """
         self.parsed_data = parsed_data
 
     def parse_postcodes(self) -> dict:
         """
         Parse data from parsed_data list into dictionary using postcode as key and nasted dictionary as value. Nasted dictionary has two keys - Latitude and Longitude and their value is of type float.
 
+        Input:
+        parsed_data: list
+        Input list must not be empty - check before further process with parse_orderbook method and raises error if it is empty.
+        The list must have these fields - validation of numeric and date fields is checked by private methods
+        Expected fields:
+        "Postcode": "string",
+        "Latitude": "Decimal place number", "must be in range from -90 to +90"
+        "Longitude": "Decimal place number", "must be in range from -180 to +180"
+
         Returns:
             dictionary: An postcode dictionary with postcode as key and value of nasted dictionary with Latitude and Longitude as key and value of float type.
         """
-        # Initialize dict datastructure which stores inventry key value pairs and also variable correct_keys
-        postcodes, correct_keys = dict(), ["Postcode", "Latitude", "Longitude"]
-        fields = {
-            "Postcode": "string",
-            "Latitude": "Decimal place number",
-            "Longitude": "Decimal place number",
-        }
-        range = {
-            "Latitude": "must be in range from -90 to +90",
-            "Longitude": "must be in range from -180 to +180",
-        }
-
-        # Check if parsed_data is not empty list
-        if self.parsed_data == []:
-            raise WrongKeysError(
-                method_called="parse_postcodes", correct_keys=correct_keys
-            )
+        # Check if input is not empty list
+        self._check_empty_list()
+        # Initialize empty postocdes dict
+        postcodes = dict()
 
         # Iterate over each item of the parsed_data list
         for row in self.parsed_data:
             # Check if row only contains correct keys
-            if sorted(list(set(key for key in row.keys()))) != sorted(correct_keys):
-                raise WrongKeysError(
-                    method_called="parse_postcodes", correct_keys=correct_keys
-                )
+            self._check_correct_keys(row)
 
-            # Check if Latitude and Longitude has decimal place numeric value. If has, convert to float data type, if not, raise error
-            # latitude_range = [-90, 90]
-            # longitude_range = [-180, 180]
-            try:
-                lat = float(row["Latitude"])
-                if lat > 90 or lat < -90:
-                    raise WrongNumericRange("Latitude", range)
-            except ValueError:
-                raise WrongValueTypeError("Latitude", fields)
-            try:
-                long = float(row["Longitude"])
-                if long > 180 or long < -180:
-                    raise WrongNumericRange("Longitude", range)
-            except ValueError:
-                raise WrongValueTypeError("Longitude", fields)
+            # Check if Latitude and Longitude has correct value
+            lat = self._validate_number_value(row["Latitude"], "Latitude")
+            long = self._validate_number_value(row["Longitude"], "Longitude")
 
             # Initialize nasted dict inside postcodes dict
             postcodes[row["Postcode"]] = dict()
@@ -61,3 +59,40 @@ class ProcessPostcodes:
             postcodes[row["Postcode"]]["Longitude"] = long
 
         return postcodes
+
+    def _check_empty_list(self) -> None:
+        """
+        Private method checking if input list is not empty. If it is, it raises error. For error details, please refer to errors.py file.
+        """
+        if self.parsed_data == []:
+            raise WrongKeysError(
+                method_called="parse_postcodes", correct_keys=self.CORRECT_KEYS
+            )
+
+    def _check_correct_keys(self, row: dict) -> None:
+        """
+        Private method checking if every row(dictionary) in the input list has correct keys. If not, it raises error. For error details, please refer to errors.py file.
+        """
+        if sorted(list(set(key for key in row.keys()))) != sorted(self.CORRECT_KEYS):
+            raise WrongKeysError(
+                method_called="parse_postcodes", correct_keys=self.CORRECT_KEYS
+            )
+
+    def _validate_number_value(self, value: str, field: str) -> float:
+        """
+        Private method checking if input value can be converted to floating point number and whether the number is in expected range. If any of these fails, it raises appropriate error. For error details, please refer to errors.py file.
+        """
+        try:
+            value = float(value)
+            if field == "Latitude":
+                if value > 90 or value < -90:
+                    raise WrongNumericRange(field, self.RANGE)
+                else:
+                    return value
+            elif field == "Longitude":
+                if value > 180 or value < -180:
+                    raise WrongNumericRange(field, self.RANGE)
+                else:
+                    return value
+        except ValueError:
+            raise WrongValueTypeError(field, self.FIELDS)
