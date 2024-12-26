a6c19f4384b19879609c73bba4eae5ffa4c24292
StefanGlova <glovastefan@protonmail.com>
2024-12-26 23:24:53 +0000

    new test created for simple inventory allocation, one order, one sku, but with not enough inventory to test allocated volume re-calculation. Test failed.

--- tests/test_allocate_inventory/test_inventory_allocation.py
+++ tests/test_allocate_inventory/test_inventory_allocation.py
@@ -102,6 +102,46 @@ class TestInventoryAllocation(unittest.TestCase):
         self.assertEqual(inventory_left["SKU2"], 100)
         self.assertEqual(orderbook_not_allocated["trailer"], [])
 
+
+    def test_inventory_allocation_simple_case_no_enough_inventory_volume_recalculation(self):
+        """
+        Test case similar to simple case, but with not enough inventory to test correct recalulation of allocated volume
+        """
+
+        # Create orderbook sample with just one order
+        orderbook = {
+            "trailer": [
+                {
+                    "Customer Name": "ABC",
+                    "Customer Postcode": "ABC123",
+                    "SKU": "SKU1",
+                    "Qty": 60,
+                    "Due Date": 2023 - 11 - 10,
+                    "Transport Volume (m3)": 60
+                },
+            ]
+        }
+
+        # Create inventory sample with just one sku
+        inventory = {
+            "SKU1": 50,
+        }
+
+        # Create allocator as object of InventoryAllocation class and run allocate_inventory method on the created object
+        allocator = InventoryAllocation(orderbook, inventory)
+        orderbook_allocated, inventory_left, orderbook_not_allocated = (
+            allocator.allocate_inventory()
+        )
+
+        # Verify the outcome of allocate_inventory method
+        self.assertEqual(orderbook_allocated["trailer"][0]["Allocated Qty"], 50)
+        self.assertEqual(orderbook_allocated["trailer"][0]["Allocated Volume"], 50)
+        self.assertEqual(inventory_left["SKU1"], 0)
+        self.assertEqual(orderbook_not_allocated["trailer"], [])
+
+
+
+
     # def test_inventory_allocation_complex_case(self):
     #     """ """
     #     print("test Inventory Allocation complex case")
