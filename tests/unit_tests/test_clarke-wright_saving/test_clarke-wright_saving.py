7885227c3963e908c128b447a68f7b8585567483
StefanGlova <glovastefan@protonmail.com>
2024-12-27 22:30:23 +0000

    more complex test with repeating postcodes and different vehicle types. Test passed without change of src.

--- tests/unit_tests/test_clarke-wright_saving/test_clarke-wright_saving.py
+++ tests/unit_tests/test_clarke-wright_saving/test_clarke-wright_saving.py
@@ -34,7 +34,83 @@ class TestClarkeWrightSavingCalculator(unittest.TestCase):
         self.assertEqual(postcodes["ABC123"]["Latitude"], 1.123456)
         self.assertEqual(postcodes["ABC123"]["Longitude"], 50.123456)
 
+    def test_used_postcodes_complex(self):
 
+        all_postcodes = {
+            "ABC123": {"Latitude": 1.123456, "Longitude": 50.123456},
+            "DEF123": {"Latitude": 1.987654, "Longitude": 50.654987},
+            "IJK123": {"Latitude": 1.123456, "Longitude": 50.123456},
+            "LMN123": {"Latitude": 1.987654, "Longitude": 50.654987},
+            "OPQ123": {"Latitude": 1.123456, "Longitude": 50.123456},
+            "RST123": {"Latitude": 1.987654, "Longitude": 50.654987},            
+        }
+
+        orderbook = {
+            "trailer": [
+                {
+                    "Customer Name": "ABC",
+                    "Customer Postcode": "ABC123",
+                    "Total Volume": 5,
+                    "Line Details": {
+                        "SKU": "SKU1",
+                        "Qty": 60,
+                        "Due Date": 2023 - 11 - 10,
+                        "Allocated Volume": 5,
+                    },
+                },
+                {
+                    "Customer Name": "DEF",
+                    "Customer Postcode": "DEF123",
+                    "Total Volume": 5,
+                    "Line Details": {
+                        "SKU": "SKU1",
+                        "Qty": 60,
+                        "Due Date": 2023 - 11 - 10,
+                        "Allocated Volume": 5,
+                    },
+                },
+            ],
+            "rigid": [
+                {
+                    "Customer Name": "ABC",
+                    "Customer Postcode": "IJK123",
+                    "Total Volume": 5,
+                    "Line Details": {
+                        "SKU": "SKU1",
+                        "Qty": 60,
+                        "Due Date": 2023 - 11 - 10,
+                        "Allocated Volume": 5,
+                    },
+                },
+                {
+                    "Customer Name": "DEF",
+                    "Customer Postcode": "ABC123",
+                    "Total Volume": 5,
+                    "Line Details": {
+                        "SKU": "SKU1",
+                        "Qty": 60,
+                        "Due Date": 2023 - 11 - 10,
+                        "Allocated Volume": 5,
+                    },
+                },
+            ],            
+        }
+
+        saver = ClarkeWrightSavingCalculator(all_postcodes, orderbook)
+        postcodes = saver.select_used_postcodes()
+
+        self.assertIn("ABC123", postcodes)
+        self.assertIn("DEF123", postcodes)
+        self.assertIn("IJK123", postcodes)
+        self.assertNotIn("LMN123", postcodes)
+        self.assertNotIn("OPQ123", postcodes)
+        self.assertNotIn("RST123", postcodes)
+        self.assertEqual(postcodes["ABC123"]["Latitude"], 1.123456)
+        self.assertEqual(postcodes["ABC123"]["Longitude"], 50.123456)
+        counter = 0
+        for postcode in postcodes:
+            counter += 1
+        self.assertEqual(counter, 3)
 
 if __name__ == "__main__":
     unittest.main()
