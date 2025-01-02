ecb0e2a70ddb7d48e71b40ad0fe8a90a264777f2
StefanGlova <glovastefan@protonmail.com>
2025-01-02 22:48:44 +0000

    test with more then one postcode where not all postcodes are same. All tests passed without change of src.

--- tests/unit_tests/test_clarke_wright_vehicle/test_clarke_wright_vehicle.py
+++ tests/unit_tests/test_clarke_wright_vehicle/test_clarke_wright_vehicle.py
@@ -100,5 +100,65 @@ class TestClarkeWrightVehiclePlanner(unittest.TestCase):
         self.assertEqual(postcodes_count["ABC123"], 4)
 
 
+    def test_postcodes_count_more_postcodes_different(self):
+        """
+        Test count of more then one postcode if they are not all same.
+        """
+
+
+        multidrop_loads_trailers = [
+            {
+                "Customer Name": "ABC",
+                "Customer Postcode": "ABC123",
+                "Total Volume": 5,
+                "Line Details": {
+                    "SKU": "SKU1",
+                    "Qty": 60,
+                    "Due Date": 2023 - 11 - 10,
+                    "Allocated Volume": 5,
+                },
+            },
+            {
+                "Customer Name": "DEF",
+                "Customer Postcode": "ABC123",
+                "Total Volume": 5,
+                "Line Details": {
+                    "SKU": "SKU1",
+                    "Qty": 60,
+                    "Due Date": 2023 - 11 - 10,
+                    "Allocated Volume": 5,
+                },
+            },
+            {
+                "Customer Name": "GHI",
+                "Customer Postcode": "ABC123",
+                "Total Volume": 5,
+                "Line Details": {
+                    "SKU": "SKU1",
+                    "Qty": 60,
+                    "Due Date": 2023 - 11 - 10,
+                    "Allocated Volume": 5,
+                },
+            },
+            {
+                "Customer Name": "JKL",
+                "Customer Postcode": "JKL123",
+                "Total Volume": 5,
+                "Line Details": {
+                    "SKU": "SKU1",
+                    "Qty": 60,
+                    "Due Date": 2023 - 11 - 10,
+                    "Allocated Volume": 5,
+                },
+            },
+        ]
+
+        planner = ClarkeWrightVehiclePlanner()
+        postcodes_count = planner.count_postcodes(orderbook=multidrop_loads_trailers)
+
+        self.assertEqual(len(postcodes_count), 2)
+        self.assertEqual(postcodes_count["ABC123"], 3)
+        self.assertEqual(postcodes_count["JKL123"], 1)
+
 if __name__ == "__main__":
     unittest.main()
