c92d8a28d586db1c773934c58134622f0347768c
StefanGlova <glovastefan@protonmail.com>
2025-01-02 22:37:21 +0000

    new failing test for method count_postcodes in class ClarkeWrightVehiclePlanner which is not implemented yet.

--- tests/unit_tests/test_clarke_wright_vehicle/test_clarke_wright_vehicle.py
+++ tests/unit_tests/test_clarke_wright_vehicle/test_clarke_wright_vehicle.py
@@ -1,7 +1,42 @@
 import unittest
 
-class TestClarkeWrightVehiclePlanner(unittest.TestCase):
-    """
-    """
 
-    
+# Testing parameters
+TRAILER_MAX = 55
+TRAILER_MIN = 50
+RIGID_MAX = 25
+RIGID_MIN = 20
+PARCEL_LIMIT = 2
+
+
+class TestClarkeWrightVehiclePlanner(unittest.TestCase):
+    """ """
+
+    def test_postcodes_count_one_postcode(self):
+        """
+        Test method postcodes_count which loops through orderbook and count how many times each postcode is used.
+        """
+
+        multidrop_loads_trailers = [
+            {
+                "Customer Name": "ABC",
+                "Customer Postcode": "ABC123",
+                "Total Volume": 5,
+                "Line Details": {
+                    "SKU": "SKU1",
+                    "Qty": 60,
+                    "Due Date": 2023 - 11 - 10,
+                    "Allocated Volume": 5,
+                },
+            },
+        ]
+
+        planner = ClarkeWrightVehiclePlanner()
+        postcodes_count = planner.count_postcodes(orderbook=multidrop_loads_trailers)
+
+        self.assertEqual(len(postcodes_count), 1)
+        self.assertEqual(postcodes_count["ABC123"], 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
